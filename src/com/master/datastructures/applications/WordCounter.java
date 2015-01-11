a8ace35d202556f249fc770a9e480b064e64069e
Simon Sharudin <simon_sharudin1@msn.com>
2015-01-11 15:14:24 -0800

    Added WordCounter app     - counts occurrences of words in a file     - uses TreeMap for key/ value pairs     - displays table of counts to words

---
+++ src/com/master/datastructures/applications/WordCounter.java
@@ -0,0 +1,71 @@
+
+package com.master.datastructures.applications;
+
+import java.util.*; // Provides TreeMap, Iterator, and Scanner
+import java.io.*;   // Provides FileReader and FileNotFoundException
+
+/**
+ * A simple program that shows the use of TreeMaps and Iterators.
+ * The program opens and reads a file called words.txt.
+ * Each line in this file should consist of one or more English words separated by spaces.
+ * The nd of each line must not have any extra spaces after the last word.
+ * The program reads the file, and then a table is printed of all words and their counts.
+ * Credit goes to Michael Main.
+ * 
+ * @author simon_000
+ */
+public class WordCounter {
+    public static void main(String[] args){
+        TreeMap<String, Integer> frequencyData = new TreeMap<>();
+        
+        readWordFile(frequencyData);
+        printAllCounts(frequencyData);
+    }
+    
+    private static int getCount(String word, TreeMap<String, Integer> frequencyData){
+        if(frequencyData.containsKey(word)){
+            // The word has occurred before, so get its count from the map
+            return frequencyData.get(word); // auto-unboxed
+        }
+        else{
+            // No occurrences of this word
+            return 0;
+        }
+    }
+    
+    private static void printAllCounts(TreeMap<String, Integer> frequencyData){
+        System.out.println("----------------------------");
+        System.out.println("    Occurrences    Word");
+        
+        for(String word : frequencyData.keySet()){
+            System.out.printf("%15d    %s\n", frequencyData.get(word), word);
+        }
+        
+        System.out.println("----------------------------");
+    }
+    
+    private static void readWordFile(TreeMap<String, Integer> frequencyData){
+        Scanner wordFile;
+        String word;    // A word read from the file
+        Integer count;  // The number of occurrences of the word
+        
+        try{
+            // Try to open the words.txt file:
+            wordFile = new Scanner(new FileReader("words.txt"));
+        }
+        catch(FileNotFoundException e){
+            // Ff the file failed, then print an error message and return without counting words
+            System.err.println(e);
+            return;
+        }
+        
+        while(wordFile.hasNext()){
+            // Read the next word and get rid of the end-of-line marker if needed
+            word = wordFile.next();
+            
+            // Get the current count of this word, add 1, and then store the new count
+            count = getCount(word, frequencyData) + 1;  // autobox
+            frequencyData.put(word, count);
+        }
+    }
+}
